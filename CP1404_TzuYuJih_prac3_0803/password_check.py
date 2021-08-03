044f97152de99512b1796263d52c1b1fee088440
TzuYuJih <tzuyu.jih@my.jcu.edu.au>
2021-08-03 17:30:13 +0800

    'Refactor password check program to use function :)'

--- CP1404_TzuYuJih_prac3_0803/password_check.py
+++ CP1404_TzuYuJih_prac3_0803/password_check.py
@@ -1,7 +1,20 @@
-password_limit = 5
-password = str(input('Please enter your password: '))
-if len(password) < password_limit :
-    print("Error : Your password length doesn't meet the minimum")
-else:
-    for i in range (len(password)) :
+def main():
+    password_limit = 5
+    password = get_password()
+    if len(password) < password_limit :
+        print("Error : Your password length doesn't meet the minimum")
+    else:
+        print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
         print('*', end='')
+
+
+def get_password():
+    password = str(input('Please enter your password: '))
+    return password
+
+
+main()
