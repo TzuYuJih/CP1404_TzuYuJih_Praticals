e6a189707d1296082ddf54a17ac2420333489ed6
TzuYuJih <tzuyu.jih@my.jcu.edu.au>
2021-08-03 17:40:05 +0800

    'Refactor some codes in temperature.py'

--- CP1404_TzuYuJih_prac3_0803/temperature.py
+++ CP1404_TzuYuJih_prac3_0803/temperature.py
@@ -11,19 +11,30 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = celsius_to_fahrenheit(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
             # TODO: Write this section to convert F to C and display the result
             # Hint: celsius = 5 / 9 * (fahrenheit - 32)
             # Remove the "pass" statement when you are done. It's a placeholder.
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit -32)
+            celsius = fahrenheit_to_celsius(celsius, fahrenheit)
             print("Result: {:.2f}".format(celsius))
         else:
             print("Invalid option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def fahrenheit_to_celsius(celsius, fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
 
 main()
