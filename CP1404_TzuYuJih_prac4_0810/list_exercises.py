503163b562ab271bcd9c30dacde4201bf180ef43
TzuYuJih <tzuyu.jih@my.jcu.edu.au>
2021-08-31 19:34:19 +0800

    finish the part1 of prac06

--- CP1404_TzuYuJih_prac4_0810/list_exercises.py
+++ CP1404_TzuYuJih_prac4_0810/list_exercises.py
@@ -1,23 +1,25 @@
-numbers = [0, 0, 0, 0, 0]
-numbers[0] = int(input("Please enter the first number: "))
-numbers[1] = int(input("Please enter the second number: "))
-numbers[2] = int(input("Please enter the third number: "))
-numbers[3] = int(input("Please enter the fourth number: "))
-numbers[4] = int(input("Please enter the fifth number: "))
-print("The first number is {}".format(numbers[0]))
-print("The last number is {}".format(numbers[-1]))
-print("The smallest number is {}".format(min(numbers)))
-print("The largest number is {}".format(max(numbers)))
-print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+def main():
+    numbers = [0, 0, 0, 0, 0]
+    numbers[0] = int(input("Please enter the first number: "))
+    numbers[1] = int(input("Please enter the second number: "))
+    numbers[2] = int(input("Please enter the third number: "))
+    numbers[3] = int(input("Please enter the fourth number: "))
+    numbers[4] = int(input("Please enter the fifth number: "))
+    print("The first number is {}".format(numbers[0]))
+    print("The last number is {}".format(numbers[-1]))
+    print("The smallest number is {}".format(min(numbers)))
+    print("The largest number is {}".format(max(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
 
-print("------------")
+    print("------------")
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+                 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-ask_username = str(input("Please enter your name: "))
-if ask_username in usernames:
-    print("Access granted")
-else:
-    print("Access denied")
+    ask_username = str(input("Please enter your name: "))
+    if ask_username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+main()
 
--- CP1404_TzuYuJih_prac4_0810/subject_reader.py
+++ CP1404_TzuYuJih_prac4_0810/subject_reader.py
@@ -14,7 +14,7 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    list = []
+    list1 = []
     for line in input_file:
         # print(line)  # See what a line looks like
         # print(repr(line))  # See what a line really looks like
@@ -23,9 +23,9 @@ def get_data():
         # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         # print(parts)  # See if that worked
-        list += [parts]
+        list1.append(parts)
         print('{} is taught by {} and has {} students'.format(parts[0], parts[1], parts[2]))
-    print(list)
+    print(list1)
 
     input_file.close()
 
