311cd772f54378377714140ff1a63cad7b85d560
TzuYuJih <tzuyu.jih@my.jcu.edu.au>
2021-09-07 20:34:07 +0800

    I checked the solution which provided in the demo files it shows that We will need to do a try catch to avoid ValueError so I did a same one as the content in the demo solution first

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -8,4 +8,15 @@ class convert_miles_km_app(App):
         self.title = "Convert miles to km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+    def handle_increment(self, value):
+
+    def handle_convert(self, value):
+        result = value *1.61
+        self.root.ids.output_label.text = str(result)
+    def get_vaild_mile(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
 convert_miles_km_app().run()
