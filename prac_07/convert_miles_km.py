40898f4a15f7668f5a8dace28c49d3e9b0c73033
TzuYuJih <tzuyu.jih@my.jcu.edu.au>
2021-09-07 20:47:06 +0800

    change the 1.60934 into a CONSTANT

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,19 +1,23 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-
+convert_m_to_km = 1.60934
 class convert_miles_km_app(App):
     def build(self):
         Window.size = (300, 200)
         self.title = "Convert miles to km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
-    def handle_increment(self, value):
-        print()
+    def handle_increment(self, change):
+        value = self.get_validated_mile() + change
+        self.root.ids.input_number.text = str(value)
+        self.handle_convert()
 
-    def handle_convert(self, value):
-        result = value *1.61
+    def handle_convert(self):
+        value = self.get_validated_mile()
+        result = value * convert_m_to_km
         self.root.ids.output_label.text = str(result)
+
     def get_validated_mile(self):
         try:
             value = float(self.root.ids.input_number.text)
